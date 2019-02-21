b9b0dfb795572e6694f1395631553b507e22e34c
Vince GRAICS <Vince.GRAICS@nng.com>
2019-02-21 15:16:18 +0100

    successfully implemented predicate expected conditions to elementfinder

--- specs/examples/google.spec.ts
+++ specs/examples/google.spec.ts
@@ -1,7 +1,6 @@
-import { browser, by, element, ElementFinder, ExpectedConditions, until } from 'protractor';
+import { browser, by, element, ExpectedConditions as EC } from 'protractor';
 import '../../utils/locators';
 import '../../utils/wait-methods';
-import { u } from '../../utils/wait-methods';
 
 
 describe('wait on google page', () => {
@@ -12,24 +11,8 @@ describe('wait on google page', () => {
         browser.get('https://www.google.com');
     });
 
-    it('should work with until callback', () => {
-        callbackWait(until.elementIsVisible, searchBtn)
-    });
-
-    it('should work with ExpectedConditions', () => {
-        browser.wait(ExpectedConditions.visibilityOf(searchBtn), 1_000)
-    });
-
-    it('should work with EC callback', () => {
-        callbackWait(ExpectedConditions.visibilityOf, searchBtn)
-    });
-
-    fit('asd', () => {
-        searchBtn.waitFor(u.visible)
+    it('should work with a predicate expected condition visibility', () => {
+        searchBtn.waitFor(EC.visibilityOf)
     })
 
 });
-
-function callbackWait(condition, element: ElementFinder) {
-    browser.wait(condition(element.getWebElement()), 1_000)
-}
